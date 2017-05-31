425271904450e223adc1ae5d62acd549118ab875
alairfonseca <alairfonseca13@gmail.com>
2017-05-31 00:48:18 -0300

    primeira controller

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+  $scope.foto = {
+    titulo : 'jo soares',
+    url : 'http://empiricusimagens.s3.amazonaws.com/imagens/copys/fn07/02.jpg'
+  };
+
+});
